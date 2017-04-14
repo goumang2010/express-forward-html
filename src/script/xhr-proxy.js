9455dd577315dd96057cfb2c8c20ab17d831ecb8
goumang2010 <goumang2010@live.com>
2017-04-14 10:42:02 +0800

    script compatibility

--- src/script/xhr-proxy.js
+++ src/script/xhr-proxy.js
@@ -1,24 +1,26 @@
 function inject(pageUrl, platform, origin, prefix, extraScript) {
-    if(platform === 'H5') {
-        Object.defineProperty(window.navigator,'userAgent' ,{writable:true, configurable: true, enumerable:true});
-        window.navigator.userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1';
+    if (window.XMLHttpRequest) {
+        if (platform === 'H5') {
+            Object.defineProperty(window.navigator, 'userAgent', { writable: true, configurable: true, enumerable: true });
+            window.navigator.userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1';
+        }
+        var open = window.XMLHttpRequest.prototype.open;
+        window.XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
+            var args = [].slice.call(arguments);
+            //  remove same origin
+            url = url.replace(origin, '');
+            if (/https?:\/\//.test(url)) {
+                if (url.match(/\.js\??/)) {
+                    args[1] = `${prefix}/js?url=${encodeURIComponent(url)}`;
+                } else {
+                    args[1] = url;
+                }
+            } else {
+                args[1] = `${prefix}/ajax` + (url.indexOf('/') === 0 ? url : '/' + url);
+            }
+            // call original open method
+            return open.apply(this, args);
+        }
     }
     extraScript(pageUrl, platform, origin, prefix);
-    var open = XMLHttpRequest.prototype.open;
-    XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
-        var args = [].slice.call(arguments);
-        //  remove same origin
-        url = url.replace(origin, '');
-        if (/https?:\/\//.test(url)) {
-            if(url.match(/\.js\??/)) {
-                args[1] = `${prefix}/js?url=${encodeURIComponent(url)}`;
-            } else {
-                args[1] = url;
-            }
-        } else {
-            args[1] = `${prefix}/ajax` + (url.indexOf('/') === 0  ? url : '/' + url);
-        }
-        // call original open method
-        return open.apply(this, args);
-    }
 }
