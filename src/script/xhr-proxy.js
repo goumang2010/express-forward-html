7a6ba17fcc1b6118c9345a4c5372f4173a0bf6f3
goumang2010 <goumang2010@live.com>
2017-05-25 18:41:03 +0800

    eliminate local href

--- src/script/xhr-proxy.js
+++ src/script/xhr-proxy.js
@@ -9,15 +9,17 @@ function inject(urlObj, extraScript) {
         window.XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
             var args = [].slice.call(arguments);
             //  remove same origin
-            url = url.replace(origin, '');
-            let res;
-            if ((res = /^(https?:)?\/\//.exec(url)) && method.toLowerCase() === 'get') {
-                if(!res[1]) {
-                    url = protocol + url;
+            if(url.indexOf(window.location.host) === -1) {
+                url = url.replace(origin, '');
+                let res;
+                if ((res = /^(https?:)?\/\//.exec(url)) && method.toLowerCase() === 'get') {
+                    if(!res[1]) {
+                        url = protocol + url;
+                    }
+                    args[1] = `${serverBase}static?url=${encodeURIComponent(url)}`;
+                } else {
+                    args[1] = `${serverBase}ajax?url=${encodeURIComponent(url)}&referer=${encodeURIComponent(urlObj.href)}`;
                 }
-                args[1] = `${serverBase}static?url=${encodeURIComponent(url)}`;
-            } else {
-                args[1] = `${serverBase}ajax?url=${encodeURIComponent(url)}&referer=${encodeURIComponent(urlObj.href)}`;
             }
             // call original open method
             return open.apply(this, args);
