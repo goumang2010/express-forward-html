85d9aa8a2393877b759f7fdae7a9449ac6945a2c
goumang2010 <goumang2010@live.com>
2017-07-11 16:10:29 +0800

    fix res adapter

--- src/adapter/response.ts
+++ src/adapter/response.ts
@@ -2,7 +2,7 @@ import { ServerResponse } from 'https';
 import { Response } from 'node-fetch-custom';
 
 export default (res: Response, nodeRes: ServerResponse) => {
-    nodeRes.statusCode = nodeRes.statusCode;
+    nodeRes.statusCode = res.status;
     res.headers.forEach((val, name) => {
         nodeRes.setHeader(name, val);
     });
