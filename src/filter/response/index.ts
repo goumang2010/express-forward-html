7e01cdb819638898178625ee3e709da5c97fb317
goumang2010 <goumang2010@live.com>
2017-07-11 16:09:30 +0800

    trim response header

--- src/filter/response/index.ts
+++ src/filter/response/index.ts
@@ -2,8 +2,10 @@ import { Options } from '../../interface';
 import { Response } from 'node-fetch-custom';
 import { idFunc } from '../../constants';
 import { transformCookie } from './cookie';
+import { trimHeader } from './header';
 export const combineResponseFilter = ({ requestFilter = idFunc }: Partial<Options> = {}) => async (res: Response) => {
     res = await requestFilter(res);
     transformCookie(res);
+    trimHeader(res);
     return res;
 };
