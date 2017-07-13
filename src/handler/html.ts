6bac59a6fbb7b60c5d7b8488ab7f4da8722d007a
goumang2010 <goumang2010@live.com>
2017-07-13 10:55:04 +0800

    redirect html when relocation found

--- src/handler/html.ts
+++ src/handler/html.ts
@@ -1,35 +1,27 @@
 import * as urlLib from 'url';
+import * as querystring from 'querystring';
 import * as fs from 'fs';
 import * as path from 'path';
 import { Handler } from '../interface';
-import { Request } from 'node-fetch-custom';
 const xhrProxy = fs.readFileSync(path.resolve(__dirname, '../script/xhr-proxy.js'), { encoding: 'utf8' }).replace(`'use strict';`, '');
 
 const htmlHandler: Handler = ({ prefix, script, requestAdapter, responseAdapter, applyCommonFilter, filterHtml }) => async (req, res, next) => {
     let finalReq = requestAdapter(req);
     let url = finalReq.url;
-    const result = await (async () => {
-        finalReq.redirect = 'manual';
-        // 先请求一次，探查真实地址
-        let _result = await applyCommonFilter(finalReq);
-        let relocation = _result.headers.get('location');
-        if (relocation && (relocation !== url)) {
-            const newReq = new Request(relocation, finalReq);
-            // copy other props
-            Object.keys(finalReq).forEach(x => {
-                if (newReq[x] == null) {
-                    newReq[x] = finalReq[x];
-                }
-            });
-            newReq.redirect = 'follow';
-            finalReq = newReq;
-            url = relocation;
-            return (await applyCommonFilter(newReq));
-        } else {
-            return _result;
-        }
-    })();
 
+    finalReq.redirect = 'manual';
+    // 先请求一次，探查真实地址
+    const result = await applyCommonFilter(finalReq);
+    let relocation = result.headers.get('location');
+    if (relocation && (relocation !== url)) {
+        if (finalReq.originalUrlObj) {
+            let query = Object.assign({}, finalReq.originalUrlObj.query, { url: relocation });
+            finalReq.originalUrlObj.search = '?' + querystring.stringify(query);
+            res.writeHead(302, { Location: urlLib.format(finalReq.originalUrlObj) });
+            res.end();
+            return;
+        }
+    }
     const serverUrlObj = finalReq.originalUrlObj;
     const urlObj = { ...urlLib.parse(url), mobile: finalReq['mobile'], UA: finalReq.headers.get('User-Agent'), prefix, serverUrlObj };
     let proxytext = `<script>(${xhrProxy}(${JSON.stringify(urlObj)}, ${script}))</script>`;
